b4e09a80ea39fab9d8b8153ed13fdf1f26d79b4a
Matt Luedke <mluedke2@gmail.com>
2016-05-12 13:35:13 -0700

    use before_destroy to validate instead of overriding destroy to follow convention

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,17 +3,22 @@ class Project < ActiveRecord::Base
   has_many :tasks
   has_many :users, through: :project_users
 
+  before_destroy :validate_no_started_or_finished_tasks
+
   accepts_nested_attributes_for :tasks, :allow_destroy => true
 
-  def destroy
-    if canBeDestroyed
-      super
-    else
-      raise "can't destroy"
+  def validate_no_started_or_finished_tasks
+    unless can_be_destroyed
+      # errors.add_to_base() is deprecated in Rails 3. Instead do...
+      self.errors.add(:base, "can't destroy")
+
+      puts self.errors.inspect
+      # this will prevent the object from getting destroyed
+      return false
     end
   end
 
-  def canBeDestroyed
+  def can_be_destroyed
     for task in self.tasks
       if task.started? || task.finished?
         return false
