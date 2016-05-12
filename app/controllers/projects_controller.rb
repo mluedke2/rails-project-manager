6e5e34754d386b8cb86160cbf9571ffaae5a91b6
Matt Luedke <mluedke2@gmail.com>
2016-05-12 13:40:13 -0700

    require project user for edit, update, destroy

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update, :destroy]
 
   # GET /projects
   # GET /projects.json
@@ -67,6 +68,10 @@ class ProjectsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_project
       @project = Project.find(params[:id])
+    end
+
+    def require_user
+      redirect_to('/') unless @project.users.include?(current_user)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
